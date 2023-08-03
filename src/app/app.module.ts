5124fdc984d57f4bc018793512bac8a475d480d3
Mike Oliveira <mikeoliveira@outlook.com>
2023-08-03 15:10:43 -0300

    refactor(remove-pipe-module): removido instancia duplicada do pipe no modulo principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { DotToCommaPipe } from './pipes/dot-to-comma.pipe';
 import { ManagerCheckInOutComponent } from './manager-check-in-out/manager-check-in-out.component';
 
 @NgModule({
-  declarations: [AppComponent, DotToCommaPipe, ManagerCheckInOutComponent],
+  declarations: [AppComponent, ManagerCheckInOutComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
