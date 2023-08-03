b70e1b8fab3509e4cfd64e2829218f9243e4a34e
Mike Oliveira <mikeoliveira@outlook.com>
2023-08-03 00:23:30 -0300

    feat(create-service-indice): adicionado servico de dados sobre updated do indices

--- src/app/services/indice/indice.service.ts
+++ src/app/services/indice/indice.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { IndiceWrapper } from 'src/app/indice/models/indiceModel';
+import { IndiceWrapper } from 'src/app/indice/models/indice.model';
 
 @Injectable({
   providedIn: 'root',
